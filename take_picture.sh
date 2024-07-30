a6312d44d9ceb09e30ac574c36135d4f7b08e219
William Fowler <williamfowler04@gmail.com>
2024-07-30 10:25:19 -0500

    original one, but with libcamera--still

--- take_picture.sh
+++ take_picture.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-# MQTT broker details
-MQTT_BROKER="0a611c2211104d4c82b15ec089b0ab68.s1.eu.hivemq.cloud"
-MQTT_USERNAME="will"
-MQTT_PASSWORD="PlayBallGame83"
-MQTT_TOPIC="picamera/image"
-MQTT_PORT=30001 
+# # MQTT broker details
+# MQTT_BROKER="0a611c2211104d4c82b15ec089b0ab68.s1.eu.hivemq.cloud"
+# MQTT_USERNAME="will"
+# MQTT_PASSWORD="PlayBallGame83"
+# MQTT_TOPIC="picamera/image"
+# MQTT_PORT=30001 
 
-while true; do
-    # Take a picture
-    # rpicam-still -o /usr/src/app/image.png --width 1920 --height 1080
-    libcamera-hello --nopreview
+# while true; do
+#     # Take a picture
+#     # rpicam-still -o /usr/src/app/image.png --width 1920 --height 1080
+#     libcamera-hello --nopreview
 
-    # # Publish the image to the MQTT broker
-    # mosquitto_pub -h "$MQTT_BROKER" -p "$MQTT_PORT" -u "$MQTT_USERNAME" -P "$MQTT_PASSWORD" -t "$MQTT_TOPIC" -f /usr/src/app/image.png
+#     # # Publish the image to the MQTT broker
+#     # mosquitto_pub -h "$MQTT_BROKER" -p "$MQTT_PORT" -u "$MQTT_USERNAME" -P "$MQTT_PASSWORD" -t "$MQTT_TOPIC" -f /usr/src/app/image.png
     
 
-    # Wait for 60 seconds
+#     # Wait for 60 seconds
+#     sleep 60
+# done
+
+while true; do
+    libcamera-still -o /usr/src/app/image.png --width 1920 --height 1080
+
     sleep 60
 done
