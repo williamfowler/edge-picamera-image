89cce8ba4fd8e222007f5237fceafe4f2c3aa642
William Fowler <williamfowler04@gmail.com>
2024-07-31 13:12:53 -0500

    ls the directory

--- take_picture.sh
+++ take_picture.sh
@@ -10,7 +10,7 @@ MQTT_PORT=1883
 while true; do
     libcamera-still --nopreview -o /usr/src/app/image.png --width 1920 --height 1080
 
-    ls
+    ls /usr/src/app/
 
     mosquitto_pub -h "$MQTT_BROKER" -p "$MQTT_PORT" -u "$MQTT_USERNAME" -P "$MQTT_PASSWORD" -t "$MQTT_TOPIC" -f /usr/src/app/image.png
 
