6ef8e8576ed36b8849b3a9d4a04924de300edca3
William Fowler <williamfowler04@gmail.com>
2024-07-31 13:04:39 -0500

    upload picture to Chameleon node

--- take_picture.sh
+++ take_picture.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 
-# # MQTT broker details
-# MQTT_BROKER="0a611c2211104d4c82b15ec089b0ab68.s1.eu.hivemq.cloud"
-# MQTT_USERNAME="will"
-# MQTT_PASSWORD="PlayBallGame83"
-# MQTT_TOPIC="picamera/image"
-# MQTT_PORT=30001 
-
-# while true; do
-#     # Take a picture
-#     # rpicam-still -o /usr/src/app/image.png --width 1920 --height 1080
-#     libcamera-hello --nopreview
-
-#     # # Publish the image to the MQTT broker
-#     # mosquitto_pub -h "$MQTT_BROKER" -p "$MQTT_PORT" -u "$MQTT_USERNAME" -P "$MQTT_PASSWORD" -t "$MQTT_TOPIC" -f /usr/src/app/image.png
-    
-
-#     # Wait for 60 seconds
-#     sleep 60
-# done
+# MQTT broker details
+MQTT_BROKER="192.5.85.172"
+MQTT_USERNAME="will"
+MQTT_PASSWORD="PlayBallGame83"
+MQTT_TOPIC="picamera/image"
+MQTT_PORT=1883 
 
 while true; do
-    # libcamera-still --nopreview -o /usr/src/app/image.png --width 1920 --height 1080
-    libcamera-hello
+    libcamera-still --nopreview -o /usr/src/app/image.png --width 1920 --height 1080
+
+    ls
+
+    mosquitto_pub -h "$MQTT_BROKER" -p "$MQTT_PORT" -u "$MQTT_USERNAME" -P "$MQTT_PASSWORD" -t "$MQTT_TOPIC" -f /usr/src/app/image.png
+
     sleep 60
 done
