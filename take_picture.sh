72a468753b74effcb7bbef926c0491ceabaa0f27
William Fowler <williamfowler04@gmail.com>
2024-07-31 13:39:54 -0500

    just try getting sending over MQTT to work

--- take_picture.sh
+++ take_picture.sh
@@ -4,15 +4,16 @@
 MQTT_BROKER="192.5.86.172"
 MQTT_USERNAME="will"
 MQTT_PASSWORD="PlayBallGame83"
-MQTT_TOPIC="picamera/image"
-MQTT_PORT=30001
+MQTT_TOPIC="test/topic"
+MQTT_PORT=1883
 
 while true; do
-    libcamera-still --nopreview -o /usr/src/app/image.png --width 1920 --height 1080
+    # libcamera-still --nopreview -o /usr/src/app/image.png --width 1920 --height 1080
 
-    ls /usr/src/app/
+    # ls /usr/src/app/
 
-    mosquitto_pub -h "$MQTT_BROKER" -p "$MQTT_PORT" -u "$MQTT_USERNAME" -P "$MQTT_PASSWORD" -t "$MQTT_TOPIC" -f /usr/src/app/image.png
+    # mosquitto_pub -h "$MQTT_BROKER" -p "$MQTT_PORT" -u "$MQTT_USERNAME" -P "$MQTT_PASSWORD" -t "$MQTT_TOPIC" -f /usr/src/app/image.png
+    mosquitto_pub -h "$MQTT_BROKER" -p "$MQTT_PORT" -u "$MQTT_USERNAME" -P "$MQTT_PASSWORD" -t "$MQTT_TOPIC" -m "hello from FLOTO!"
 
     sleep 60
 done
